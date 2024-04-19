4f5af5a191197a259f53d7862be6ab57372a369b
Dani Lyne <DLyne@pm.me>
2024-04-19 15:29:40 -0400

    Set up project scaffold

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "simple-chat dev-shell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+
+        devShells.default = mkShell {
+          buildInputs = [
+            # Rust build requirements
+            openssl
+            pkg-config
+
+            # Rust and LSP
+            rust-bin.stable.latest.default
+            rust-analyzer
+
+            # Other
+            buf-language-server
+            protobuf
+          ];
+
+          shellHook = ''
+          '';
+        };
+      }
+    );
+}
